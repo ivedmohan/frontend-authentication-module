4368a6f1205eccbbd17997c4f1429e431919d6ea
ivedmohan <vedmohan0@gmail.com>
2024-04-25 01:00:31 +0530

    improved frontend

--- frontend-auth-module/projects/sl-auth-library/src/lib/components/login/login.component.ts
+++ frontend-auth-module/projects/sl-auth-library/src/lib/components/login/login.component.ts
@@ -1,28 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'lib-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+
+
 })
 export class LoginComponent implements OnInit {
-  loginusers: any = [];
-  signupobj: any = {
-    name: '',
-    email: '',
-    password: '',
-  };
-  loginobj: any = {
-    email: '',
-    password: ''
-  };
-
-  constructor() { }
-  onlogin() {
-    this.loginusers.push(this.loginobj);
-    localStorage.setItem('loginusers', JSON.stringify(this.loginusers));
+  constructor() {
+    
   }
   ngOnInit(): void {
+    
   }
-
+  
 }
+
+
